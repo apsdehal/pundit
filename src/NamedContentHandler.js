6061b4c554542876361481f23b0a0986372ac209
Christian Morbidoni <christian.morbidoni@gmail.com>
2014-03-07 13:08:35 +0100

    fixe local/global variables

--- src/NamedContentHandler.js
+++ src/NamedContentHandler.js
@@ -110,7 +110,7 @@ dojo.declare("pundit.NamedContentHandler", pundit.BaseComponent, {
                         return function(ld) {
                             var label = jsonld.getValues(ld, 'http://www.w3.org/2000/01/rdf-schema#label')[0],
                                 types = jsonld.getValues(ld, '@type'),
-                                description = jsonld.getValues(ld, 'http://purl.org/dc/elements/1.1/description')[0];
+                                description = jsonld.getValues(ld, 'http://purl.org/dc/elements/1.1/description')[0],
                                 image = jsonld.getValues(ld, 'http://xmlns.com/foaf/0.1/depiction')[0];
 
                             for (var l=types.length; l--;) 
